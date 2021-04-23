bd5b6d3fa9f87336605be84a7b0f88b54c609537
Lucas Vieira <lucas@devplace.com.br>
2021-04-23 10:34:28 -0300

    Adjust env vars

--- src/services.ts
+++ src/services.ts
@@ -6,18 +6,18 @@ export const sendTemplateEmail = async () => {};
 
 export class EmailService {
   private readonly client = new SQS({
-    region: process.env.AWS_REGION || 'us-east-1',
+    region: process.env.AWS_MAILER_REGION || 'us-east-1',
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+      accessKeyId: process.env.AWS_MAILER_ACCESS_KEY_ID as string,
+      secretAccessKey: process.env.AWS_MAILER_SECRET_ACCESS_KEY as string,
     },
   });
 
   async sendSingleTemplateEmail(params: SendSingleTemplateEmailInput) {
-    const {AWS_SQS_FIFO_QUEUE_URL, APP_NAME = 'mailer'} = process.env;
+    const {AWS_MAILER_SQS_FIFO_QUEUE_URL, APP_NAME = 'mailer'} = process.env;
 
     const command = new SendMessageCommand({
-      QueueUrl: AWS_SQS_FIFO_QUEUE_URL,
+      QueueUrl: AWS_MAILER_SQS_FIFO_QUEUE_URL,
       MessageDeduplicationId: uuid(),
       MessageGroupId: APP_NAME,
       MessageBody: JSON.stringify(params),
